237a945f47eb194b8bb2bfff386864f926d32a12
thiyangi28 <thiyangip24@gmail.com>
2025-11-19 22:14:08 +0530

    changes on pacakges

---
+++ src/main/java/org/example/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package org.example.controller;
+import org.example.model.dto.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.example.service.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("attendance")
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @PostMapping("/punch")
+    public  void addAttendance(@RequestBody Employee employee){
+        employeeService.punch(employee);
+
+    }
+    @GetMapping("all")
+    public List<Employee> showAll(){
+        return employeeService.getAll();
+
+    }
+//        public List<Book> getAllDetails(){
+//            return bookService.getAllDetails();
+//    }
+
+}
